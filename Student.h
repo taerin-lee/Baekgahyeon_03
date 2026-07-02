8a5ffe9cb8c2d6d54aac15cdfe4cc14fbac9e231
taerin-lee <flsxodl@ewhain.net>
2026-07-02 10:38:42 +0900

    lab4 생성자
    Refactor student constructor to include default parameters and call validation methods.

--- Student.h
+++ Student.h
@@ -13,8 +13,13 @@ namespace LeeTaerin2276249
         void testScore();
         void calGrade();
 
-    public:     
-
+    public:   
+        student(int i=2276249 , int s=0 , char g='F' ) : id(i), score(s), grade(g) {
+            testId();
+            testScore();
+            calGrade();
+        } //기본값이 있는 student 생성자. calGrade(); 있으니까 char g, grade(g) 없어도 된다고..? 근데 값을 집어넣어주고 싶으니까 냅두어라..?
+        
         void input();
         void print();
         void setId(int id);
